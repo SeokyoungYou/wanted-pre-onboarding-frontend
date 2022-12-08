350bd988790759a089061fa3620c850021ffe66a
Quartz <skyu.dev@gmail.com>
2022-12-08 19:20:12 +0900

    style: add global style and theme colors

--- to-do-app/src/routes/Home.js
+++ to-do-app/src/routes/Home.js
@@ -1,7 +1,29 @@
+import styled from 'styled-components';
+import { theme } from '../theme';
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.bgColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const SignInWrapper = styled.div`
+  background-color: white;
+  width: 80%;
+  height: 80%;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+`;
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Wrapper bgColor={theme.bgColorlight}>
+      <SignInWrapper>
+        <Title>Homeee</Title>
+      </SignInWrapper>
+    </Wrapper>
   );
 }
---
+++ to-do-app/src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  bgColorlight: '#EFF5F5',
+  bgColordark: '#D6E4E5',
+  pointColor: '#EB6440',
+  btnColor: '#497174',
+  textColor: 'black',
+  grey: 'rgba(0,0,0,0.3)',
+};
